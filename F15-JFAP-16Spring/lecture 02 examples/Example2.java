a08a17908037415d842bd7864fa7c5f6697e03ad
dotdot1123 <ruiw1123@gmail.com>
2016-05-12 22:07:33 -0400

    change files

--- F15-JFAP-16Spring/lecture 02 examples/Example2.java
+++
@@ -1,6 +0,0 @@
-public class Example2 {
-    public static void main(String[] args) {
-        final String BLOOD_TYPE = "A" + "B";
-        BLOOD_TYPE = "O";
-    }
-}
--- F15-JFAP-16Spring/lecture 02 examples/Example4.java
+++
@@ -1,5 +0,0 @@
-public class Example4 {
-    public static void main(String[] args) {
-        System.out.println(args[0]);
-    }
-}
